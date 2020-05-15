fa99db271a9583abfff6b922f2fb78d78be8b4c6
reischc <cedric.reisch@ict-campus.net>
2020-05-15 15:53:00 +0200

    14.05.2020, 15:55, first button Form done

--- app/src/main/java/com/rimbo/NewReminder.java
+++ app/src/main/java/com/rimbo/NewReminder.java
@@ -3,9 +3,12 @@ package com.rimbo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
-public class NewReminder extends AppCompatActivity {
+public class NewReminder extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,31 @@ public class NewReminder extends AppCompatActivity {
         CheckBox checkBoxImportance = (CheckBox) findViewById(R.id.checkBoxImportance);
         CheckBox checkBoxKindOfReminder = (CheckBox) findViewById(R.id.checkBoxKind);
 
+        checkBoxLocation.setOnCheckedChangeListener(this);
+        checkBoxVehicle.setOnCheckedChangeListener(this);
+        checkBoxImportance.setOnCheckedChangeListener(this);
+        checkBoxKindOfReminder.setOnCheckedChangeListener(this);
+    }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.checkBoxLocation:
+                EditText txtLocation = (EditText) findViewById(R.id.txtLocation);
+                if (isChecked) {
+                    txtLocation.setVisibility(View.VISIBLE);
+                } else {
+                    txtLocation.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.checkBoxVehicle:
+                break;
+            case R.id.checkBoxImportance:
+                break;
+            case R.id.checkBoxKind:
+                break;
+            default:
+                break;
+        }
     }
 }
