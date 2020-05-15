2e05763dfcfb094a49acaca1748bf8dfa6b342a9
reischc <cedric.reisch@ict-campus.net>
2020-05-15 14:55:11 +0200

    14.05.2020, 14:55, start Form

--- app/src/main/java/com/rimbo/NewReminder.java
+++ app/src/main/java/com/rimbo/NewReminder.java
@@ -3,6 +3,7 @@ package com.rimbo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class NewReminder extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class NewReminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_reminder);
+
+        CheckBox checkBoxLocation = (CheckBox) findViewById(R.id.checkBoxLocation);
+        CheckBox checkBoxVehicle = (CheckBox) findViewById(R.id.checkBoxVehicle);
+        CheckBox checkBoxImportance = (CheckBox) findViewById(R.id.checkBoxImportance);
+        CheckBox checkBoxKindOfReminder = (CheckBox) findViewById(R.id.checkBoxKind);
 
 
     }
