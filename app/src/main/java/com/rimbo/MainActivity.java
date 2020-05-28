1b8e767ca0e2dad53378cee392ccd6db0d26097a
reischc <cedric.reisch@ict-campus.net>
2020-05-28 15:32:18 +0200

    28.05.2020, 15:55, add Reminder finished

--- app/src/main/java/com/rimbo/MainActivity.java
+++ app/src/main/java/com/rimbo/MainActivity.java
@@ -8,14 +8,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ListView listViewReminder;
+    private List<Reminder> allReminder = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnCalendar = (Button)findViewById(R.id.btnCalendar);
         this.listViewReminder = (ListView)findViewById(R.id.listViewReminder);
 
-
+        //set listView mod
+        this.listViewReminder.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         //activate click Listener
         btnCalendar.setOnClickListener(this);
+
+        this.listViewReminder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CheckedTextView v = (CheckedTextView) view;
+                boolean currentCheck = v.isChecked();
+
+            }
+        });
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -53,9 +72,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return false;
             }
         });
+        loadReminder();
+        initListViewData();
     }
 
+    public void initListViewData() {
+        String[] reminderNameList = new String[]{};
+        String name;
+        if (allReminder.size() != 0) {
+            for (int i = 0; i <= allReminder.size(); i++) {
+                name = allReminder.get(i).getName();
+                reminderNameList[i] = name;
+            }
+        }
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked , reminderNameList);
+        this.listViewReminder.setAdapter(arrayAdapter);
+
+    }
+
+    public void loadReminder() {
+        SQLite db = new SQLite(this);
+        allReminder = db.getAllReminder();
+    }
 
 
     @Override
@@ -71,10 +110,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         finish();
     }
 
-
-
-        /*---------------------------
-                    Menu
-        ---------------------------*/
 }
 
