7cde3e50e696a7b6e701034c5e4a52961a228ed7
reischc <cedric.reisch@ict-campus.net>
2020-05-28 08:03:24 +0200

    28.05.2020, 08:05, start with SQLite

--- app/src/main/java/com/rimbo/SQLite.java
+++ app/src/main/java/com/rimbo/SQLite.java
@@ -1,10 +1,15 @@
 package com.rimbo;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLite extends SQLiteOpenHelper {
 
@@ -22,6 +27,7 @@ public class SQLite extends SQLiteOpenHelper {
     private static final String ColumnVehicle = "Vehicle";
     private static final String ColumnImportance = "Importance";
     private static final String ColumnTimer = "Timer";
+    private static final String ColumnDone = "Done";
 
     public SQLite(@Nullable Context context) {
         super(context, databaseName, null, databaseVersion);
@@ -29,7 +35,7 @@ public class SQLite extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String script = "Create table"+tableName+"("+ColumnIDNote+"Integer Primary key"+ColumnName+"String"+ColumnDate+"String"+ColumnTime+"String"+ColumnLocation+"String"+ColumnVehicle+"String"+ColumnImportance+"String"+ColumnTimer+"String"+")";
+        String script = "Create table"+tableName+"("+ColumnIDNote+"Integer Primary key autoo_increment"+ColumnName+"String"+ColumnDate+"String"+ColumnTime+"String"+ColumnLocation+"String"+ColumnVehicle+"String"+ColumnImportance+"String"+ColumnTimer+"String"+ColumnDone+"boolean"+")";
         db.execSQL(script);
     }
 
@@ -38,5 +44,38 @@ public class SQLite extends SQLiteOpenHelper {
 
     }
 
-    
+    public void addReminder(Reminder reminder) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        //fill values with content
+        values.put(ColumnName, reminder.getName());
+        values.put(ColumnDate, reminder.getDate());
+        values.put(ColumnTime, reminder.getTime());
+        values.put(ColumnLocation, reminder.getLocation());
+        values.put(ColumnVehicle, reminder.getVehicle());
+        values.put(ColumnImportance, reminder.getImportanceLevel());
+        values.put(ColumnTimer, reminder.getTimer());
+        values.put(ColumnDone, reminder.isDone());
+
+        //insert values into db
+        db.insert(tableName, null, values);
+
+        //close Connection
+        db.close();
+    }
+
+    public List<Reminder> getReminder() {
+        List<Reminder> allReminder = new ArrayList<>();
+        String selectQuery = "Select * from"+tableName;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if(cursor.moveToFirst()) {
+            do {
+                Reminder reminder = new Reminder(cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7), Boolean.parseBoolean(String.valueOf(cursor.getInt(8))));
+
+            } while(cursor.moveToNext());
+        }
+    }
 }
