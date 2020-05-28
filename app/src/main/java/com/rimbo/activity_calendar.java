91bf9af0d3a01d8bfbcabd37c0dd4fbe42345b27
Levin Hochreutener <levin.hochreutener@icloud.com>
2020-05-28 13:11:40 +0200

    28.05.2020, 11:51

--- app/src/main/java/com/rimbo/activity_calendar.java
+++ app/src/main/java/com/rimbo/activity_calendar.java
@@ -1,7 +1,11 @@
 package com.rimbo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_calendar extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class activity_calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        Button btnBack = (Button) findViewById(R.id.button_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityCalendarIntent = new Intent(getApplicationContext(), activity_calendar.class);
+                startActivity(activityCalendarIntent);
+            }
+        });
     }
 }
